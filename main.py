ef988e3f5dbe5d0d6e970a443ea5d87abdbcebd1
Quentin Joubert <quentin.joubert28@gmail.com>
2020-10-29 15:05:44 +0100

    Add Swagger

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 import json
 
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-from flask_swagger import swagger
+from flask_restplus import Api, Resource
 import requests
+from DB import wrapperDB
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-api = Api(app)
+api = Api(app,
+          version="1.0",
+          title="Closing API",
+          description="Api of MicroService Closing")
+
+name_space = api.namespace('closing', description='closing APIs')
 
 
-@app.route('/closing', methods=['GET'])
-def closingsAll():
-    # result = getAllContracts()
-    result = {'data': 'closing get'}
-    return jsonify(result)
+@api.route('/closing')
+class closings(Resource):
+    def get(self):
+        """Returns list of closing."""
+        # result = getAllContracts()
+        result = {'data': 'closing get'}
+        return jsonify(result)
+
+    def post(self):
+        """close a contract."""
+        content = request.json
+        code_contract = content['code_contract']
+        user_name = content['user_name']
+        response = wrapperDB.updateStateClose(code_contract, user_name)
+        if response == -1:
+            return {'error': 'wrong request'}, 503
+        result = json.dumps(response),
+        # changeContract(code_contract)
+        return jsonify(result)
 
 
-@app.route('/closing/<int:code>', methods=['GET'])
-def closingid(code):
-    # result = getContractByCode(code)
-    result = {'data': 'code ' + str(code)}
-    return jsonify(result)
-
-
-@app.route('/closing', methods=['POST'])
-def closingsAdd():
-    content = request.json
-    code_contract = content['code_contract']
-    user_name = content['user_name']
-    # response = wrapperDB.updateStateClose(code_contract, user_name)
-    response = {'timestamp': 3578, 'user': 2, 'change_success': 0}
-    if response == -1:
-        return {'error': 'wrong request'}
-    result = json.dumps(response),
-    # changeContract(code_contract)
-    return jsonify(result)
+@api.route('/closing/<code>')
+class closingid(Resource):
+    def get(self, code):
+        """Returns a closing."""
+        # result = getContractByCode(code)
+        result = {'data': 'code ' + code}
+        return jsonify(result)
 
 
 def changeContract(code):
-    response = requests.put('http://127.0.0.1:8091/api/v1/deals/close/' + str(code), data={'code': code})
+    response = requests.put('http://127.0.0.1:8091/api/v1/deals/close/' + code, data={'code': code})
     return response.status_code
 
 
 if __name__ == '__main__':
+    wrapperDB.initDB()
     app.run(port='8093')
